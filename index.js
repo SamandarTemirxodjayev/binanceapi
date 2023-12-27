52d2b0d5cc691ac094752270b5d5bc650009e8cc
Samandar <iwtuz166@gmail.com>
2023-12-27 12:59:27 +0500

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+async function getSpotBalance(apiKey, secretKey) {
+ const timestamp = Date.now();
+ const signature = Buffer.from(
+    require('crypto')
+      .createHmac('sha256', secretKey)
+      .update(`${timestamp}`)
+      .digest('hex')
+ ).toString('base64');
+
+ const url = 'https://api.binance.com/api/v3/account';
+ const params = {
+    timestamp,
+    signature,
+ };
+
+ const headers = {
+    'X-MBX-APIKEY': apiKey,
+ };
+
+ try {
+    const response = await axios.get(url, { params, headers });
+    const balanceData = response.data.balances;
+
+    // filter out spot balance data
+    const spotBalanceData = balanceData.filter(
+      (data) => data.asset !== 'MATIC' && data.asset !== 'XRP'
+    );
+
+    // print balance data
+    console.log('Spot Balance Data:', spotBalanceData);
+ } catch (error) {
+    console.error('Error:', error);
+ }
+}
+
+// usage
+const apiKey = 'lpW2CW5FOmb9aSd6AcXKV2NktJD0ZaGvIMFWNtZPrjPdINncHCkWPwmvI2t1je3i';
+const secretKey = 'UjXiXouKmVWViYrqLm4ljyTrC8BwGy3iDyltL4RYZCpflYQeVRhfgzeglToM7wRr';
+getSpotBalance(apiKey, secretKey);
