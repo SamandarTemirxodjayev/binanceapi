b33b4615c9d8bbb18135c1a3a699475449789a2d
Samandar <iwtuz166@gmail.com>
2023-12-30 22:59:25 +0500

    #

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-const axios = require('axios');
-const crypto = require('crypto');
+const { Spot } = require('@binance/connector')
+const apiKey = 'p5DUGzmBYoBQ0w4NsUeyJpmpQKvzj362LJ2atw0sCWuoW2atugYlffooiAVwo2Ir'
+const apiSecret = 'dcfvUiCGiy1rVsUQVWWjlWIifKZzEd31xvI0ClOs1iBLopasFHNLI2KZ0RKaRHbM'
+const client = new Spot(apiKey, apiSecret)
 
-async function getSpotBalance(apiKey, secretKey) {
- const timestamp = Date.now();
- const signature = crypto
-    .createHmac('sha256', secretKey)
-    .update(`${timestamp}`)
-    .digest('hex');
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.json(client.account().then(response => client.logger.log(response.data)))
+});
 
- const url = 'https://api.binance.com/api/v3/account';
- const params = {
-    timestamp,
-    signature,
- };
-
- const headers = {
-    'X-MBX-APIKEY': apiKey,
- };
-
- try {
-    const response = await axios.get(url, { params, headers });
-    const balanceData = response.data.balances;
-
-    // filter out spot balance data
-    const spotBalanceData = balanceData.filter(
-      (data) => data.asset !== 'MATIC' && data.asset !== 'XRP'
-    );
-
-    // print balance data
-    console.log('Spot Balance Data:', spotBalanceData);
- } catch (error) {
-    console.error('Error:', error);
- }
-}
-
-// usage
-const apiKey = 'lpW2CW5FOmb9aSd6AcXKV2NktJD0ZaGvIMFWNtZPrjPdINncHCkWPwmvI2t1je3i';
-const secretKey = 'UjXiXouKmVWViYrqLm4ljyTrC8BwGy3iDyltL4RYZCpflYQeVRhfgzeglToM7wRr';
-getSpotBalance(apiKey, secretKey);
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
